1dd6d8e21675915f3dd9ccbc3cf71ffc31301727
Monica Gondolini <monica.gondolini@studio.unibo.it>
2019-04-09 14:29:05 +0200

    Edited ClientMain.

--- client/src/main/scala/ClientMain.scala
+++ client/src/main/scala/ClientMain.scala
@@ -3,12 +3,12 @@ import javafx.fxml.FXMLLoader
 import javafx.scene.{Parent, Scene}
 import javafx.stage.Stage
 
-object ClientMain extends Application {
+class ClientMain extends Application {
 
     override def start(primaryStage: Stage): Unit = {
       val root: Parent = FXMLLoader.load(getClass.getClassLoader.getResource("fxml/main_view.fxml"))
       val scene : Scene = new Scene(root)
-      primaryStage.setTitle("Login")
+      primaryStage.setTitle("Akka Chat")
       primaryStage.setScene(scene)
       primaryStage.setResizable(false)
       primaryStage.setOnCloseRequest(_ => {
